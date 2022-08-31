c3fd802941664c9bdb11c91b57d19d4092e06b6b
Marc Arbones Tutusaus <marbonestu@gmail.com>
2022-08-31 12:24:08 -0400

    update fzflua

--- nvim/.config/nvim/lua/config/fzf.lua
+++ nvim/.config/nvim/lua/config/fzf.lua
@@ -14,14 +14,21 @@ function M.setup()
       stat_file               = true,
       include_current_session = true,
     },
+    git_bcommits = {
+      preview = { layout = 'horizontal' }
+    },
+    git_diff = {
+      pager = "delta --width $FZF_PREVIEW_COLUMNS"
+    }
   }
 
 
-  vim.keymap.set('n', '<leader>f', function() require('fzf-lua').files() end)
-  vim.keymap.set('n', '<leader>sb', function() require('fzf-lua').buffers() end)
-  vim.keymap.set('n', '<leader>sr', function() require('fzf-lua').oldfiles() end)
-  vim.keymap.set('n', '<leader>st', function() require('fzf-lua').live_grep() end)
+  vim.keymap.set('n', '<leader>f', function() require('fzf-lua').files({ continue_last_search = true }) end)
+  vim.keymap.set('n', '<leader>sb', function() require('fzf-lua').buffers({ continue_last_search = true }) end)
+  vim.keymap.set('n', '<leader>sr', function() require('fzf-lua').oldfiles({ continue_last_search = true }) end)
+  vim.keymap.set('n', '<leader>st', function() require('fzf-lua').live_grep({ continue_last_search = true }) end)
   vim.keymap.set('n', '<leader>sp', function() require('fzf-lua').resume() end)
+  vim.keymap.set('n', '<leader>gb', function() require('fzf-lua').git_bcommits() end)
 
 end
 
