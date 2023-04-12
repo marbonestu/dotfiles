5ee80270baee6b35cec5c4a77e512f36b1b57a28
Marc Arbones Tutusaus <marbonestu@gmail.com>
2023-04-12 09:22:30 -0400

    update tree config after nvim upgrade

--- nvim/.config/nvim/lua/config/tree.lua
+++ nvim/.config/nvim/lua/config/tree.lua
@@ -4,15 +4,14 @@ function M.setup()
   local setup = {
     disable_netrw = true,
     hijack_netrw = true,
-    open_on_setup = false,
-    open_on_setup_file = false,
+    -- open_on_setup_file = false,
     sort_by = "name",
-    ignore_buffer_on_setup = false,
-    ignore_ft_on_setup = {
-      "startify",
-      "dashboard",
-      "alpha",
-    },
+    -- ignore_buffer_on_setup = false,
+    -- ignore_ft_on_setup = {
+    --   "startify",
+    --   "dashboard",
+    --   "alpha",
+    -- },
     auto_reload_on_write = true,
     hijack_unnamed_buffer_when_opening = false,
     hijack_directories = {
