b98993a5c6bd7c381f3260e83cc9db17e5a87c99
Marc Arbones Tutusaus <marbonestu@gmail.com>
2023-09-04 06:17:27 -0300

    update window picker

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -41,7 +41,32 @@ return {
               buftype = { "terminal", "quickfix", "nofile" },
             },
           },
-          hint = "floating-big-letter",
+          picker_config = {
+            statusline_winbar_picker = {
+              -- You can change the display string in status bar.
+              -- It supports '%' printf style. Such as `return char .. ': %f'` to display
+              -- buffer file path. See :h 'stl' for details.
+              selection_display = function(char, windowid)
+                return "%=" .. char .. "%="
+              end,
+
+              -- whether you want to use winbar instead of the statusline
+              -- "always" means to always use winbar,
+              -- "never" means to never use winbar
+              -- "smart" means to use winbar if cmdheight=0 and statusline if cmdheight > 0
+              use_winbar = "always", -- "always" | "never" | "smart"
+            },
+
+            floating_big_letter = {
+              -- window picker plugin provides bunch of big letter fonts
+              -- fonts will be lazy loaded as they are being requested
+              -- additionally, user can pass in a table of fonts in to font
+              -- property to use instead
+
+              font = "ansi-shadow", -- ansi-shadow |
+            },
+          },
+          hint = "statusline-winbar",
           highlights = {
             statusline = {
               focused = {
@@ -50,8 +75,8 @@ return {
                 bold = true,
               },
               unfocused = {
-                fg = "#ededed",
-                bg = "#000000",
+                fg = "#cdd6f4",
+                bg = "#0064a3",
                 bold = true,
               },
             },
@@ -63,7 +88,7 @@ return {
               },
               unfocused = {
                 fg = "#cdd6f4",
-                bg = "#45475a",
+                bg = "#0064a3",
                 bold = true,
               },
             },
@@ -131,6 +156,91 @@ return {
     end,
   },
   {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+
+      signs = {
+        add = { hl = "GitSignsAdd", text = "▍", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        change = {
+          hl = "GitSignsChange",
+          text = "▍",
+          numhl = "GitSignsChangeNr",
+          linehl = "GitSignsChangeLn",
+        },
+        delete = {
+          hl = "GitSignsDelete",
+          text = "▸",
+          numhl = "GitSignsDeleteNr",
+          linehl = "GitSignsDeleteLn",
+        },
+        topdelete = {
+          hl = "GitSignsDelete",
+          text = "▾",
+          numhl = "GitSignsDeleteNr",
+          linehl = "GitSignsDeleteLn",
+        },
+        changedelete = {
+          hl = "GitSignsChange",
+          text = "▍",
+          numhl = "GitSignsChangeNr",
+          linehl = "GitSignsChangeLn",
+        },
+      },
+
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "<leader>gj", function()
+          if vim.wo.diff then
+            return "]c"
+          end
+          vim.schedule(function()
+            gs.next_hunk()
+          end)
+          return "<Ignore>"
+        end, { expr = true })
+
+        map("n", "<leader>gk", function()
+          if vim.wo.diff then
+            return "[c"
+          end
+          vim.schedule(function()
+            gs.prev_hunk()
+          end)
+          return "<Ignore>"
+        end, { expr = true })
+
+        -- Actions
+        map({ "n", "v" }, "<leader>g", ":Gitsigns stage_hunk<CR>")
+        map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>")
+        map("n", "<leader>gS", gs.stage_buffer)
+        map("n", "<leader>gu", gs.undo_stage_hunk)
+        map("n", "<leader>gR", gs.reset_buffer)
+        map("n", "<leader>gp", gs.preview_hunk)
+        map("n", "<leader>gb", function()
+          gs.blame_line({ full = true })
+        end)
+        map("n", "<leader>gl", gs.blame_line)
+        map("n", "<leader>gL", "<cmd>G blame<CR>")
+        map("n", "<leader>gd", gs.diffthis)
+        map("n", "<leader>gD", function()
+          gs.diffthis("~")
+        end)
+        map("n", "<leader>gd", gs.toggle_deleted)
+
+        -- Text object
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+      end,
+    },
+  },
+  {
     "folke/which-key.nvim",
     opts = {
       plugins = { spelling = true },
@@ -154,4 +264,5 @@ return {
       },
     },
   },
+  { "folke/noice.nvim", enabled = false },
 }
--- nvim/.config/nvim/lua/plugins/tools.lua
+++ nvim/.config/nvim/lua/plugins/tools.lua
@@ -79,4 +79,10 @@ return {
       })
     end,
   },
+  {
+    "ziontee113/color-picker.nvim",
+    config = function()
+      require("color-picker").setup()
+    end,
+  },
 }
